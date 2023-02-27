6b7a44368292d9195f09a76005c18c94329fb12e
Jacob Dev <mercihabam@gmail.com>
2023-02-27 17:13:01 +0200

    feat: implement calculator logic

--- src/components/Displayer.jsx
+++ src/components/Displayer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Displayer() {
+function Displayer({value}) {
   return (
-    <div className='w-full bg-gray-500 h-full col-span-4 flex justify-end items-end text-2xl text-white p-5'>Displayer</div>
+    <div className='w-full bg-gray-500 h-full col-span-4 flex justify-end items-end text-2xl text-white p-5'>{value || '0'}</div>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Displayer from './Displayer'
 import NumControl from './NumControl'
 import Operators from './Operators'
 
 function Layout() {
+    const [operation, setOperation] = useState([]);
+    const [result, setResult] = useState();
+    const [latestValue, setLatestValue] = useState()
+ 
+    const onReset = () =>{
+        setOperation([]);
+        setResult(null);
+        setLatestValue(null)
+    };
+
+    useEffect(() =>{
+        console.log(operation);
+    }, [operation])
+
+    const getResult = () =>{
+        const res = eval([...operation, latestValue].join(' '))
+        setResult(res);
+        setOperation([res]);
+    };
+
   return (
     <div className='bg-black h-screen'>
         <div className="bg-white h-full w-[500px] m-auto grid grid-rows-[160px,1fr] grid-cols-4">
-            <Displayer />
-            <NumControl />
-            <Operators />
+            <Displayer value={result || latestValue || operation.at(-1)} />
+            <NumControl setValue={setLatestValue} setOperation={setOperation} onReset={onReset} setResult={setResult} value={latestValue} operation={operation} />
+            <Operators setOperation={setOperation} getResult={getResult} setLatestValue={setLatestValue} latestValue={latestValue} operation={operation} />
         </div>
     </div>
   )
