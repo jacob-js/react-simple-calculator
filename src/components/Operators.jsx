e7053eea07189dbbc59bd5178e37dd7f734b9f0a
Jacob Dev <mercihabam@gmail.com>
2023-02-27 12:31:08 +0200

    feat: implement operator component

--- src/components/Operators.jsx
+++ src/components/Operators.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+const KEYS = [
+    {text: "÷", value: "/"},
+    {text: "x", value: "*"},
+    {text: "-", value: "-"},
+    {text: "+", value: "+"},
+    {text: "=", value: "="},
+]
 
 function Operators() {
   return (
-    <div className='bg-orange-600 h-full'>Operators</div>
+    <div className='bg-orange-600 h-full grid grid-cols-1 grid-rows-5 font-bold text-white text-2xl'>
+        {
+            KEYS.map((key, index) =>(
+                <div className={`border active:bg-orange-500 transition-all border-gray-400 w-full h-full flex justify-center items-center cursor-pointer`} key={index}>{key.text}</div>
+            ))
+        }
+    </div>
   )
 }
 
